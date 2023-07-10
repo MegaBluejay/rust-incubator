c8f9884bfa4eccc3c13c93eabe3090ff329a5b95
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-10 20:29:56 +0700

    do task 1_1_default_clone_copy

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,39 @@
+mod geo {
+    #[derive(Clone, Copy, Default, Debug)]
+    pub struct Point {
+        pub x: i32,
+        pub y: i32,
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct Polyline(Vec<Point>);
+
+    #[derive(Debug)]
+    pub enum PolylineError {
+        EmptyVec,
+    }
+
+    impl Polyline {
+        pub fn new(points: Vec<Point>) -> Result<Self, PolylineError> {
+            if points.is_empty() {
+                Err(PolylineError::EmptyVec)
+            } else {
+                Ok(Self(points))
+            }
+        }
+    }
+}
+
+use geo::*;
+
 fn main() {
-    println!("Implement me!");
+    let default_point = Point::default();
+    let point = Point { x: 1, y: 2 };
+
+    let line1 = Polyline::new(vec![default_point, point]).unwrap();
+
+    // still accesible, since Point is Copy
+    println!("{:?}", default_point);
+
+    println!("{:?}, {:?}", line1, line1.clone());
 }
