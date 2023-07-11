cb7873fe94140c5a25f1b2572867c235c1f7c0ed
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-11 20:09:50 +0700

    task 1_3

--- 1_concepts/1_3_rc_cell/src/main.rs
+++ 1_concepts/1_3_rc_cell/src/main.rs
@@ -1,3 +1,35 @@
+use gstack::GlobalStack;
+
+mod gstack {
+    use std::{cell::RefCell, rc::Rc};
+
+    pub struct GlobalStack<T>(Rc<RefCell<Vec<T>>>);
+
+    impl<T> GlobalStack<T> {
+        pub fn new() -> Self {
+            Self(Rc::new(RefCell::new(vec![])))
+        }
+
+        pub fn push(&self, item: T) {
+            self.0.borrow_mut().push(item)
+        }
+
+        pub fn pop(&self) -> Option<T> {
+            self.0.borrow_mut().pop()
+        }
+    }
+
+    impl<T> Clone for GlobalStack<T> {
+        fn clone(&self) -> Self {
+            Self(self.0.clone())
+        }
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let stack = GlobalStack::new();
+    let (stack1, stack2) = (&stack, &stack);
+    stack1.push(1);
+    stack2.push(2);
+    println!("{:?}", stack1.pop());
 }
