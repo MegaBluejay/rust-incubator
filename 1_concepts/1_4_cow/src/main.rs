de7d74ded6529019257f130c9da6ea171f7232ef
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-12 14:58:07 +0700

    task 1_4

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,30 @@
+use std::{
+    borrow::Cow,
+    env,
+    path::{Path, PathBuf},
+};
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    conf: Option<PathBuf>,
+}
+
+fn config_path(arg_conf: Option<PathBuf>) -> Cow<'static, Path> {
+    if let Some(arg_conf) = arg_conf {
+        return Cow::Owned(arg_conf);
+    }
+
+    if let Ok(env_conf) = env::var("APP_CONF") {
+        return Cow::Owned(env_conf.into());
+    }
+
+    Cow::Borrowed(Path::new("/etc/app/app.conf"))
+}
+
 fn main() {
-    println!("Implement me!");
+    let args = Args::parse();
+    println!("{:?}", config_path(args.conf));
 }
