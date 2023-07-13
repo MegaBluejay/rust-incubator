ba0230e4006a1dd54f2b19c7d7061cec3fce5f9a
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-13 18:57:44 +0700

    task 1_8

--- 1_concepts/1_8_thread_safety/src/main.rs
+++ 1_concepts/1_8_thread_safety/src/main.rs
@@ -1,3 +1,64 @@
+use std::{
+    cell::Cell,
+    marker::PhantomData,
+    rc::Rc,
+    sync::{Mutex, MutexGuard},
+    thread,
+};
+
+#[derive(Debug)]
+struct OnlySync<'a>(PhantomData<MutexGuard<'a, ()>>);
+
+#[derive(Debug)]
+struct OnlySend(PhantomData<Cell<()>>);
+
+#[derive(Debug)]
+struct SyncAndSend(PhantomData<Mutex<()>>);
+
+#[derive(Debug)]
+struct NotSyncNotSend(PhantomData<Rc<()>>);
+
 fn main() {
-    println!("Implement me!");
+    let sync = OnlySync(PhantomData);
+    let send = OnlySend(PhantomData);
+    let both = SyncAndSend(PhantomData);
+    let none = NotSyncNotSend(PhantomData);
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!("{:?}", &sync);
+        });
+
+        s.spawn(|| {
+            println!("{:?}", &both);
+        });
+
+        // s.spawn(|| {
+        //     println!("{:?}", &send);
+        // });
+
+        // s.spawn(|| {
+        //     println!("{:?}", &none);
+        // })
+    });
+
+    thread::scope(|s| {
+        s.spawn(move || {
+            println!("{:?}", send);
+        });
+
+        s.spawn(move || {
+            println!("{:?}", both);
+        });
+
+        // s.spawn(move || {
+        //     println!("{:?}", sync);
+        // });
+
+        // s.spawn(move || {
+        //     println!("{:?}", none);
+        // });
+    });
+
+    println!("{:?}", none);
 }
