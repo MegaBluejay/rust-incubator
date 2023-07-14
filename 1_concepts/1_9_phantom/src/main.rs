070de925c34a880b5cf8c050c0dbdfef180cc42e
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-14 15:06:58 +0700

    task 1_9

--- 1_concepts/1_9_phantom/src/main.rs
+++ 1_concepts/1_9_phantom/src/main.rs
@@ -1,3 +1,29 @@
+use std::{marker::PhantomData, sync::atomic::AtomicPtr};
+
+use rand::Rng;
+
+struct Fact<T: ?Sized>(PhantomData<AtomicPtr<*const T>>);
+
+trait KnownFacts {
+    const FACTS: &'static [&'static str];
+}
+
+impl<T: KnownFacts + ?Sized> Fact<T> {
+    fn new() -> Self {
+        Self(PhantomData)
+    }
+
+    fn fact(&self) -> &str {
+        T::FACTS[rand::thread_rng().gen_range(0..T::FACTS.len())]
+    }
+}
+
+impl<T> KnownFacts for Vec<T> {
+    const FACTS: &'static [&'static str] =
+        &["Vec is heap-allocated", "Vec may re-allocate on growing"];
+}
+
 fn main() {
-    println!("Implement me!");
+    let fvec: Fact<Vec<i32>> = Fact::new();
+    println!("{}", fvec.fact());
 }
