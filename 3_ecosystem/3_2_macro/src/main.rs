66ee73e5cc39f86c069dba2a1e5e07f5e4fcd3db
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-21 15:56:30 +0700

    add declerative btreemap

--- 3_ecosystem/3_2_macro/src/main.rs
+++ 3_ecosystem/3_2_macro/src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::BTreeMap;
+
+macro_rules! btreemap {
+    ($($key:expr => $val:expr),*) => {
+        {
+            #[allow(unused_mut)]
+            let mut m = BTreeMap::new();
+            $(
+                m.insert($key, $val);
+            )*
+            m
+        }
+    };
+}
+
 fn main() {
-    println!("Implement me!");
+    let m = btreemap!["a".to_owned() => 2, "b".to_owned() => 4];
+    println!("{:?}", m);
 }
