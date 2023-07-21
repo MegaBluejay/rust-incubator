7eb37d7a71e01776d015ccdabc8f3aecba4c0323
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-21 20:28:30 +0700

    construct date in User::with_birthdate

--- 3_ecosystem/3_3_date_time/src/main.rs
+++ 3_ecosystem/3_3_date_time/src/main.rs
@@ -1,14 +1,35 @@
+use std::num::TryFromIntError;
+
+use thiserror::Error;
+use time::{error::ComponentRange, Date};
+
 fn main() {
     println!("Implement me!");
 }
 
 const NOW: &str = "2019-06-26";
 
-struct User;
+struct User {
+    birthday: Date,
+}
+
+#[derive(Debug, Error)]
+enum UserError {
+    #[error("invalid date")]
+    ComponentRange(#[from] ComponentRange),
+    #[error("date component not in range")]
+    TryFromIntError(#[from] TryFromIntError),
+}
 
 impl User {
-    fn with_birthdate(year: i32, month: u32, day: u32) -> Self {
-        unimplemented!()
+    fn with_birthdate(year: i32, month: u32, day: u32) -> Result<Self, UserError> {
+        Ok(Self {
+            birthday: Date::from_calendar_date(
+                year,
+                u8::try_from(month)?.try_into()?,
+                day.try_into()?,
+            )?,
+        })
     }
 
     /// Returns current age of [`User`] in years.
@@ -27,7 +48,7 @@ mod age_spec {
     use super::*;
 
     #[test]
-    fn counts_age() {
+    fn counts_age() -> Result<(), UserError> {
         for ((y, m, d), expected) in vec![
             ((1990, 6, 4), 29),
             ((1990, 7, 4), 28),
@@ -35,21 +56,23 @@ mod age_spec {
             ((1970, 1, 1), 49),
             ((2019, 6, 25), 0),
         ] {
-            let user = User::with_birthdate(y, m, d);
+            let user = User::with_birthdate(y, m, d)?;
             assert_eq!(user.age(), expected);
         }
+        Ok(())
     }
 
     #[test]
-    fn zero_if_birthdate_in_future() {
+    fn zero_if_birthdate_in_future() -> Result<(), UserError> {
         for ((y, m, d), expected) in vec![
             ((2032, 6, 25), 0),
             ((2016, 6, 27), 0),
             ((3000, 6, 27), 0),
             ((9999, 6, 27), 0),
         ] {
-            let user = User::with_birthdate(y, m, d);
+            let user = User::with_birthdate(y, m, d)?;
             assert_eq!(user.age(), expected);
         }
+        Ok(())
     }
 }
