751dcc6e36ebec3c0545c4aaa84f8096b7e73b32
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-24 16:44:50 +0700

    add test

--- 3_ecosystem/3_6_serde/src/main.rs
+++ 3_ecosystem/3_6_serde/src/main.rs
@@ -1,15 +1,14 @@
 use std::time::Duration;
 
-use anyhow::Error;
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
 use url::Url;
 use uuid::Uuid;
 
-fn main() -> Result<(), Error> {
-    let input = include_bytes!("../request.json");
+static INPUT: &[u8] = include_bytes!("../request.json");
 
-    let req: Request = serde_json::from_slice(input)?;
+fn main() -> Result<(), anyhow::Error> {
+    let req: Request = serde_json::from_slice(INPUT)?;
 
     serde_yaml::to_writer(std::io::stdout(), &req)?;
 
@@ -21,7 +20,7 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Request {
     #[serde(rename = "type")]
     req_type: ReqType,
@@ -30,13 +29,13 @@ struct Request {
     debug: DebugInfo,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "snake_case")]
 enum ReqType {
     Success,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Stream {
     user_id: Uuid,
     is_private: bool,
@@ -46,7 +45,7 @@ struct Stream {
     private_tariff: PrivateTariff,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct PublicTariff {
     #[serde(flatten)]
     base: BaseTariff,
@@ -54,31 +53,86 @@ struct PublicTariff {
     price: u32,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct PrivateTariff {
     #[serde(flatten)]
     base: BaseTariff,
     client_price: u32,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct BaseTariff {
     #[serde(with = "humantime_serde")]
     duration: Duration,
     description: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Gift {
     id: u32,
     price: u32,
     description: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct DebugInfo {
     #[serde(with = "humantime_serde")]
     duration: Duration,
     #[serde(with = "time::serde::rfc3339")]
     at: OffsetDateTime,
+}
+
+#[cfg(test)]
+mod tests {
+    use time::macros::datetime;
+    use uuid::uuid;
+
+    use super::*;
+
+    #[test]
+    fn request() -> Result<(), anyhow::Error> {
+        let request: Request = serde_json::from_slice(INPUT)?;
+        let expected = Request {
+            req_type: ReqType::Success,
+            stream: Stream {
+                user_id: uuid!("8d234120-0bda-49b2-b7e0-fbd3912f6cbf"),
+                is_private: false,
+                settings: 45345,
+                shard_url: Url::parse("https://n3.example.com/sapi").unwrap(),
+                public_tariff: PublicTariff {
+                    base: BaseTariff {
+                        duration: Duration::from_secs(60 * 60),
+                        description: "test public tariff".to_owned(),
+                    },
+                    id: 1,
+                    price: 100,
+                },
+                private_tariff: PrivateTariff {
+                    base: BaseTariff {
+                        duration: Duration::from_secs(60),
+                        description: "test private tariff".to_owned(),
+                    },
+                    client_price: 250,
+                },
+            },
+            gifts: vec![
+                Gift {
+                    id: 1,
+                    price: 2,
+                    description: "Gift 1".to_owned(),
+                },
+                Gift {
+                    id: 2,
+                    price: 3,
+                    description: "Gift 2".to_owned(),
+                },
+            ],
+            debug: DebugInfo {
+                duration: Duration::from_millis(234),
+                at: datetime!(2019-06-28 08:35:46 UTC),
+            },
+        };
+        assert_eq!(request, expected);
+        Ok(())
+    }
 }
