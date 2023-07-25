410571dc04f5e1cd571f0f311561aa35f3c4707d
MegaBluejay <dmoiseev2011@gmail.com>
2023-07-26 00:20:03 +0300

    fix UserRepository interface

--- 3_ecosystem/3_5_collections/src/main.rs
+++ 3_ecosystem/3_5_collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use rpds::HashTrieMap;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
@@ -7,11 +9,11 @@ pub struct User {
 }
 
 trait UserRepository {
-    fn get_user(&self, id: u64) -> Option<&User>;
+    fn get_user(&self, id: u64) -> Option<User>;
 
-    fn get_users(&self, ids: impl IntoIterator<Item = u64>) -> Option<Vec<&User>>;
+    fn get_users(&self, ids: impl AsRef<[u64]>) -> HashMap<u64, User>;
 
-    fn find_users(&self, nickname: impl AsRef<str>) -> Vec<u64>;
+    fn find_users(&self, nickname: impl AsRef<str>) -> HashSet<u64>;
 }
 
 pub struct PersistentRepo {
@@ -19,15 +21,18 @@ pub struct PersistentRepo {
 }
 
 impl UserRepository for PersistentRepo {
-    fn get_user(&self, id: u64) -> Option<&User> {
-        self.users.get(&id)
+    fn get_user(&self, id: u64) -> Option<User> {
+        self.users.get(&id).cloned()
     }
 
-    fn get_users(&self, ids: impl IntoIterator<Item = u64>) -> Option<Vec<&User>> {
-        ids.into_iter().map(|id| self.get_user(id)).collect()
+    fn get_users(&self, ids: impl AsRef<[u64]>) -> HashMap<u64, User> {
+        ids.as_ref()
+            .iter()
+            .filter_map(|&id| self.get_user(id).map(|user| (id, user)))
+            .collect()
     }
 
-    fn find_users(&self, nickname: impl AsRef<str>) -> Vec<u64> {
+    fn find_users(&self, nickname: impl AsRef<str>) -> HashSet<u64> {
         self.users
             .values()
             .filter(|user| user.nickname.contains(nickname.as_ref()))
@@ -46,9 +51,6 @@ impl FromIterator<User> for PersistentRepo {
 
 #[cfg(test)]
 mod tests {
-    use archery::RcK;
-    use rpds::{ht_set, HashTrieSet};
-
     use super::*;
 
     #[test]
@@ -62,7 +64,7 @@ mod tests {
             nickname: "b".to_owned(),
         };
         let repo = PersistentRepo::from_iter([u1.clone(), u2]);
-        assert_eq!(repo.get_user(1), Some(&u1));
+        assert_eq!(repo.get_user(1), Some(u1));
     }
 
     #[test]
@@ -81,8 +83,8 @@ mod tests {
         };
         let repo = PersistentRepo::from_iter([u1.clone(), u2.clone(), u3]);
         assert_eq!(
-            repo.get_users([1, 2]).map(HashTrieSet::from_iter),
-            Some(ht_set![&u1, &u2]),
+            repo.get_users([1, 2]),
+            [u1, u2].into_iter().map(|user| (user.id, user)).collect(),
         );
     }
 
@@ -101,10 +103,7 @@ mod tests {
             nickname: "bop".to_owned(),
         };
         let repo = PersistentRepo::from_iter([u1, u2, u3]);
-        assert_eq!(
-            HashTrieSet::<_, RcK, _>::from_iter(repo.find_users("a")),
-            ht_set![1, 2]
-        );
+        assert_eq!(repo.find_users("a"), HashSet::from_iter([1, 2]));
     }
 }
 
