d7cb8b807d3863130e0f9cd86d3d0885e2de9017
MegaBluejay <dmoiseev2011@gmail.com>
2023-08-01 11:29:00 +0300

    extract create_out_file to separate module

--- 3_ecosystem/src/main.rs
+++ 3_ecosystem/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    io::{Cursor, ErrorKind},
-    path::{Path, PathBuf},
-};
+use std::{io::Cursor, path::Path};
 
 use anyhow::{anyhow, Result};
 use auto_enums::auto_enum;
@@ -22,7 +19,7 @@ use image::{
 use isahc::{prelude::Configurable, HttpClient};
 use once_cell::sync::Lazy;
 use tokio::{
-    fs::{self, File, OpenOptions},
+    fs,
     io::{AsyncReadExt, AsyncWriteExt},
 };
 use tracing::{debug_span, info, instrument, Instrument};
@@ -30,9 +27,11 @@ use tracing_subscriber::{fmt::format::FmtSpan, prelude::*, EnvFilter};
 
 use config::{Cli, Config, SourceEnum};
 use input_image::{ClientResult, InputImage};
+use out_file::create_out_file;
 
 mod config;
 mod input_image;
+mod out_file;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -171,67 +170,6 @@ async fn process_image<'a, F: FnOnce() -> ClientResult<'a>>(
         .await?;
 
     Ok(())
-}
-
-#[instrument]
-async fn create_out_file(
-    out_dir: &Path,
-    name: &str,
-    extension: &str,
-) -> Result<File, std::io::Error> {
-    let mut options = OpenOptions::new();
-    options.write(true).create_new(true);
-    for out_path in out_paths(out_dir, name, extension) {
-        match options.open(&out_path).await {
-            Ok(file) => {
-                info!(?out_path);
-                return Ok(file);
-            }
-            Err(err) => match err.kind() {
-                ErrorKind::AlreadyExists => {}
-                _ => return Err(err),
-            },
-        }
-    }
-    unreachable!()
-}
-
-struct OutNames<'a> {
-    name: &'a str,
-    extension: &'a str,
-    i: u32,
-}
-
-impl<'a> Iterator for OutNames<'a> {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let res = if self.i == 0 {
-            format!("{}.{}", self.name, self.extension)
-        } else {
-            format!("{}({}).{}", self.name, self.i, self.extension)
-        };
-        self.i += 1;
-        Some(res)
-    }
-}
-
-impl<'a> OutNames<'a> {
-    fn new(name: &'a str, extension: &'a str) -> Self {
-        Self {
-            name,
-            extension,
-            i: 0,
-        }
-    }
-}
-
-fn out_paths<'a>(
-    out_dir: &'a Path,
-    name: &'a str,
-    extension: &'a str,
-) -> impl Iterator<Item = PathBuf> + 'a {
-    OutNames::new(name, extension).map(|out_name| out_dir.join(out_name))
 }
 
 #[instrument(skip(in_data))]
